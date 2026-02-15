d37459227a420ad5007fbe89f132cdea9e46f763
illider8bread <202684742+illider8bread@users.noreply.github.com>
2026-02-15 12:09:37 -0700

    Implemented functionality to remove image from array using the "remove" button

--- sheets/src/components/InputManager.jsx
+++ sheets/src/components/InputManager.jsx
@@ -4,11 +4,15 @@ import ImagePreview from './ImagePreview'
 
 function InputManager({  photos, setPhotos }) {
     const [adding, setAdding]= useState(false)
+    function removeImage(photo){
+        const filteredPhotos = photos.filter(item => item !== photo);
+        setPhotos(filteredPhotos);
+    }
   return (
     <section className="input_manager">
         <div className="previews">
         {photos.length > 0 && photos.map((photo, index)=>(
-            <ImagePreview photo={photo} index={index} removeImage={()=>{console.log("remove image was pressed")}}/>
+            <ImagePreview photo={photo} index={index} removeImage={(image)=>{removeImage(image)}}/>
         ))}
         </div>
         {adding && <InputPopup open={setAdding} photos={photos} setPhotos={setPhotos} />}
